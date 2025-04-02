5de940c6152feea3479e5689946947da2b6b915a
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-04-02 13:08:08 -0300

    Input Signal y HostBinding

--- 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, input } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -6,12 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './calculator-button.component.html',
   styleUrl: './calculator-button.component.css',
   host: {
-    class : 'w-1/4 border-r border-b border-indigo-400'
+    class: 'w-1/4 border-r border-b border-indigo-400'
   }
 })
-export class CalculatorButtonComponent {
-// Con el "host" se le puede agregar estilos al componente desde el padre, en este caso se le agrega un borde a la derecha y abajo
-// en este caso le aplica a todo el componente, pero se puede aplicar a un elemento específico dentro del componente
-// Se puede usar para agregar clases de tailwind, pero no es necesario, ya que se pueden agregar directamente en el template
-// En este caso se le aplica a calcular-button
+export class CalculatorButtonComponent  {
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  @HostBinding('class.is-command') get commandStyle() {
+    return this.isCommand();
+  }
+  // Con el "host" se le puede agregar estilos al componente desde el padre, en este caso se le agrega un borde a la derecha y abajo
+  // en este caso le aplica a todo el componente, pero se puede aplicar a un elemento específico dentro del componente
+  // Se puede usar para agregar clases de tailwind, pero no es necesario, ya que se pueden agregar directamente en el template
+  // En este caso se le aplica a calcular-button
+
 }
