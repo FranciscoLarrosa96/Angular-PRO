36700a645b4df01ce2ae0974528cb6e670ce894a
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-09-07 17:57:24 -0300

    Construir numero y calculos

--- 01-zoneless-calculator/src/app/calculator/services/calculator.service.spec.ts
+++ 01-zoneless-calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -9,7 +9,7 @@ describe('CalculatorService', () => {
         service = TestBed.inject(CalculatorService);
     });
 
-    
+
 
     it('should be created', () => {
         expect(service).toBeTruthy();
@@ -30,5 +30,65 @@ describe('CalculatorService', () => {
         expect(service.resultTxt()).toBe('0');
         expect(service.subResultTxt()).toBe('0');
         expect(service.lastOperator()).toBe('+');
+    });
+
+    it('sould update resultTxt with number input', () => {
+        service.resultTxt.set('0');
+        service.constructNumber('5');
+        expect(service.resultTxt()).toBe('5');
+        service.constructNumber('3');
+        expect(service.resultTxt()).toBe('53');
+    });
+
+    it('should handle operators correctly', () => {
+        service.resultTxt.set('5');
+        service.subResultTxt.set('10');
+        service.lastOperator.set('+');
+        service.constructNumber('*');
+        expect(service.resultTxt()).toBe('0');
+        expect(service.subResultTxt()).toBe('15');
+        expect(service.lastOperator()).toBe('*');
+    });
+
+    it('should calculate result correctly for addition', () => {
+        service.resultTxt.set('5');
+        service.subResultTxt.set('10');
+        service.lastOperator.set('+');
+        service.constructNumber('=');
+        expect(service.resultTxt()).toBe('15');
+    });
+
+    it('should calculate result correctly for subtraction', () => {
+        service.resultTxt.set('5');
+        service.subResultTxt.set('10');
+        service.lastOperator.set('-');
+        service.constructNumber('=');
+        expect(service.resultTxt()).toBe('5');
+    });
+
+    it('should calculate result correctly for multiplication', () => {
+        service.resultTxt.set('5');
+        service.subResultTxt.set('10');
+        service.lastOperator.set('*');
+        service.constructNumber('=');
+        expect(service.resultTxt()).toBe('50');
+    });
+
+    it('should calculate result correctly for division', () => {
+        service.resultTxt.set('5');
+        service.subResultTxt.set('10');
+        service.lastOperator.set('/');
+        service.constructNumber('=');
+        expect(service.resultTxt()).toBe('2');
+    });
+
+    it('should handle decimal points correctly', () => {
+        service.resultTxt.set('0');
+        service.constructNumber('.');
+        expect(service.resultTxt()).toBe('0.');
+        service.constructNumber('5');
+        expect(service.resultTxt()).toBe('0.5');
+        service.constructNumber('.');
+        expect(service.resultTxt()).toBe('0.5.'); // No debería agregar otro punto
     });
 });
