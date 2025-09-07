d0d6ef142db7de8f5cb28295de82edcb0b94ab87
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-09-07 19:25:06 -0300

    SPA Tradicional

--- 02-pokemon-ssr/src/app/app.routes.ts
+++ 02-pokemon-ssr/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about-page/about').then(m => m.About)
+    },
+    {
+        path: 'pricing',
+        loadComponent: () => import('./pages/pricing-page/pricing-page').then(m => m.PricingPage)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact').then(m => m.Contact)
+    },
+    {
+        path: '**',
+        redirectTo: () => {
+            return 'about';
+        }
+    }
+];
