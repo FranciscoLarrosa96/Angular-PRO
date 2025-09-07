3a91a96e0f1dff2c165d70416a3dc1ec39b98a0b
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-09-07 18:15:37 -0300

    PRuebas en calculator view

---
+++ 01-zoneless-calculator/src/app/calculator/views/calculator-view/calculator-view.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import CalculatorViewComponent from "./calculator-view.component";
+
+describe('CalculatorViewComponent', () => {
+    let fixture: ComponentFixture<CalculatorViewComponent>;
+    let compiled: HTMLElement;
+    let component: CalculatorViewComponent;
+
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [CalculatorViewComponent]
+        }).compileComponents();
+        fixture = TestBed.createComponent(CalculatorViewComponent);
+        component = fixture.componentInstance;
+        compiled = fixture.nativeElement as HTMLElement;
+    });
+
+
+
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should contain calculator component', () => {
+        expect(compiled.querySelector('calculator')).not.toBeNull();
+    });
+
+    it('should contain basic css claseses', () => {
+        const divElement = compiled.querySelector('div');
+
+        const shoulHave = 'w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden';
+        expect(divElement?.className).toContain(shoulHave);
+    });
+});
