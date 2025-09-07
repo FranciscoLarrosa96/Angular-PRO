41a568e8041e23bc06207139d64c7e050b14867f
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-09-07 15:06:57 -0300

    Ciclo de vida - Reinicializacion

--- 01-zoneless-calculator/src/app/calculator/services/calculator.service.spec.ts
+++ 01-zoneless-calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -9,6 +9,8 @@ describe('CalculatorService', () => {
         service = TestBed.inject(CalculatorService);
     });
 
+    
+
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
@@ -16,6 +18,17 @@ describe('CalculatorService', () => {
     it('should be created with initial values', () => {
         expect(service.resultTxt()).toBe('10');
         expect(service.subResultTxt()).toBe('20');
+        expect(service.lastOperator()).toBe('+');
+    });
+
+    it('should set resultTxt, subResultTxt to 0 when C is pressed', () => {
+        service.resultTxt.set('12345');
+        service.subResultTxt.set('67890');
+        service.lastOperator.set('*');
+
+        service.constructNumber('C');
+        expect(service.resultTxt()).toBe('0');
+        expect(service.subResultTxt()).toBe('0');
         expect(service.lastOperator()).toBe('+');
     });
 });
