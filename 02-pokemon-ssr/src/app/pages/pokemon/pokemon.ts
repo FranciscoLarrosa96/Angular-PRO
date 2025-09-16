443bf4b5dfea9acd03579f9db9d0e0c637fe35d9
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-09-16 09:30:36 -0300

    Metatags y Title

--- 02-pokemon-ssr/src/app/pages/pokemon/pokemon.ts
+++ 02-pokemon-ssr/src/app/pages/pokemon/pokemon.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { TitleCasePipe } from '@angular/common';
 import { PokemonDetails } from '../../pokemons/interfaces/pokemonId.interface';
 import { PokemonsService } from '../../pokemons/services/pokemons';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemon',
@@ -13,9 +15,24 @@ import { PokemonsService } from '../../pokemons/services/pokemons';
 export class Pokemon implements OnInit{
   public pokemon = signal<PokemonDetails | null>(null);
   private pokemonSvc = inject(PokemonsService);
+  private title = inject(Title);
+  private meta = inject(Meta);
   idPokemon = input<string | undefined>(undefined, { alias: 'id' });
   ngOnInit(): void {
-    this.pokemonSvc.loadPokemonById(this.idPokemon()!).subscribe({
+    this.pokemonSvc.loadPokemonById(this.idPokemon()!)
+    .pipe(
+      tap(pokemon => {
+        const pageTitle = `${pokemon.id} - ${pokemon.name}`;
+        const pageDescription = `Pagina del pokemon ${pokemon.name}`;
+        this.title.setTitle(pageTitle);
+        this.meta.updateTag({ name: 'description', content: pageDescription });
+        this.meta.updateTag({ name: 'og:title', content: pageTitle });
+        this.meta.updateTag({ name: 'og:description', content: pageDescription });
+        this.meta.updateTag({ name: 'og:image', content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg` });
+
+      })
+    )
+    .subscribe({
       next: (pokemon) => {
         this.pokemon.set(pokemon);
       },
