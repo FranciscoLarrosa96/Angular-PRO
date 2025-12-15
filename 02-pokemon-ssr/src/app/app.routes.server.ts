e040736ebcde0263ae0ff54d34c500c0240f3074
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-12-15 14:00:30 -0300

    Add server route for individual Pokémon with prerendering

--- 02-pokemon-ssr/src/app/app.routes.server.ts
+++ 02-pokemon-ssr/src/app/app.routes.server.ts
@@ -2,7 +2,17 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemon/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const pokemonIds = Array.from({ length: 151 }, (_, i) => ({
+        id: `${i + 1}`,
+      }));
+      return pokemonIds;
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
