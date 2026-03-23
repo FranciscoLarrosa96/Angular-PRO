a1e4b15e42217eb62fc28752214596f20d082c2a
Francisco Larrosa <franciscolarrosa22@gmail.com>
2026-03-23 14:54:49 -0300

    Contruir routes mediante http

--- 02-pokemon-ssr/scripts/prerender-routes.js
+++ 02-pokemon-ssr/scripts/prerender-routes.js
@@ -1,7 +1,7 @@
 const TOTAL_POKEMONS = 10;
 const TOTAL_PAGES = Math.ceil(10);
 
-(() => {
+(async () => {
     const fs = require('fs');
     // Pokemons por id
     const pokemonIds = Array.from({ length: TOTAL_POKEMONS }, (_, i) => i + 1);
@@ -12,5 +12,12 @@ const TOTAL_PAGES = Math.ceil(10);
     for (let page = 1; page <= TOTAL_PAGES; page++) {
         routes += `\n/pokemons/page/${page}`;
     }
+
+    // Por nombre de pokemon
+    const pokemonNames = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${TOTAL_POKEMONS}`)
+        .then(response => response.json())
+        .then(data => data.results.map(pokemon => pokemon.name));
+    routes += '\n' + pokemonNames.map(name => `/pokemons/${name}`).join('\n');
+
     fs.writeFileSync('routes.txt', routes);
 })(); 
